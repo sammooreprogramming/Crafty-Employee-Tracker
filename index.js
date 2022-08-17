4103dd2091082daf246ca07406c33237bed2edb1
Sam Moore <sammooresemail23@gmail.com>
2022-08-17 14:17:39 -0700

    added dotenv to protect password

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+// Requirements / dependencies listed below.
 const mysql = require('mysql');
 const inquirer = require('inquirer');
-require(console.table);
+const cTable = require(console.table);
+// The below is for password encryption and security purposes.
+require('dotenv').config()
 
+// Database conncection (password hidden with dotenv)
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: process.env.CHALLENGE_PASSWORD,
+    database: 'employee_db'
+});
+
+// 
